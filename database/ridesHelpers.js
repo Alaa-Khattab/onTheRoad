54bf67f642913ff2a297278a206b3d78090a3c83
4daliaomry4 <alaakhattab92@gmail.com>
2017-05-19 17:25:59 +0300

    add ladyRides route to select all rides created by ladies

--- database/ridesHelpers.js
+++ database/ridesHelpers.js
@@ -18,7 +18,25 @@ function getRides(cb){
 
 
 }
-module.exports={
-  getRides
+function getLadyRides(cb){
+  const query = `SELECT drivers.full_name,
+    drivers.phone,
+    drivers.car_num,
+    drivers.gender,
+    trip.date,
+    trip.pick_up_time,
+    trip.pick_up_point,
+    trip.available_seats,
+    trip.price,
+    location.location_name as location_from ,
+    (select location_name from location where
+ location_id=trip.location_to_id) as location_to
+ FROM trip , drivers,location where trip.location_from_id=location.location_id AND gender=0;`;
+  dbutils.selectAll(query,cb)
+
+
 }
-  
+module.exports={
+  getRides,
+  getLadyRides
+}
