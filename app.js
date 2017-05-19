f88baa561a21f97b45fe4ffb95c9702f0989956a
4daliaomry4 <alaakhattab92@gmail.com>
2017-05-19 13:40:06 +0300

    add login route ans make session ans cookie

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const dbutils = require('./database/dbutils.js');
+var expressSession = require('express-session');
+var bodyParser = require('body-parser');
+const router = require('./backEnd/router.js');
 const app = express();
 
-const router = require('./backEnd/router.js');
 
 app.use(express.static('public'));
 app.use(cookieParser());
+app.use(cookieParser());
+app.use(expressSession({secret:'somesecrettokenhere'}));
 
 app.use(router);
 dbutils.runMigrate((error) => {
-  if (error) 
+  if (error)
     throw error
     app.listen(
       3000,
